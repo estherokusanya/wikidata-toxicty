7ccef1dc99be80dec7cc59266cc7355c983a8098
estherokusanya <49434920+estherokusanya@users.noreply.github.com>
2024-03-28 14:00:31 +0000

    Update link_prediction.py

--- link_prediction.py
+++ link_prediction.py
@@ -51,21 +51,46 @@ def remove_triple(file):
     results = []
     for item in data:
         sentence_split = item["verbalisations"].split(". ")
-        chosen = random.choice(sentence_split)
-        sentence_split.remove(chosen)
+        chosen1 = random.choice(sentence_split)
+        sentence_split.remove(chosen1)
+        chosen2 = random.choice(sentence_split)
+        sentence_split.remove(chosen2)
         results.append({
             "subset_verbalisation": sentence_split,
-            "chosen_triple": chosen,
+            "chosen_triple": [chosen1, chosen2],
             "toxicities": item["toxicities"],
             "toxic_fraction": item["toxic_fraction"],
             "sentiments": item["sentiments"]
         })
-
+        
     with open(f"link_prediction/removed_triple_{file}_results.json", "w") as f:
             json.dump(results, f, indent=2)
 
+def produce_questions(file):
+    output = []
+    context = "Create a fill in the blank question along the answer based on this sentence:"
+    with open(f"link_prediction/{file}_results.json", "r") as f:
+        data = json.load(f)
+    for item in data:
+        questions=[]
+        for i in range(2):
+            message = context + item["chosen_triple"]
+            response = client.chat.completions.create(
+                model="gpt-3.5-turbo-0125",
+                response_format={ "type": "json_object" },
+                messages=[
+                    {"role": "system", "content": "You are a helpful assistant designed to output JSON."},
+                    {"role": "user", "content": message}]
+            )
+            for i in json.loads(response.choices[0].message.content):
+                questions.append(json.loads(response.choices[0].message.content)[i])
+        item["questions"] = questions
+        output.append(item)
+    with open(f"link_prediction/{file}_questions.json", "w") as f:
+        json.dump(output, f, indent=2)
 
 for item in files:
     combine_verbalisations(item, 10)
     print("reached")
     remove_triple(item)
+    produce_questions(item)
